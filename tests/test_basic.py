20134c0f395c6b8932657e83f15350d8d1ab4bdc
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-01-28 21:02:06 +0100

    Removed dummy test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -5,8 +5,3 @@ import spatialdata_plot
 
 # def test_package_has_version():
 #     spatialdata_plot.__version__
-
-
-@pytest.mark.skip(reason="This decorator should be removed when test passes.")
-def test_example():
-    assert 1 == 0  # This test is designed to fail.
