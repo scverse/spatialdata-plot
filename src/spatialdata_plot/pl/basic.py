45e04c382fc8fb99daf9e1fd41f4a22bcf1a4582
Harald Sager Voehringer <voehring@login02.cluster.embl.de>
2023-02-06 13:49:34 +0100

    basic implementation pl.scatter

--- src/spatialdata_plot/pl/basic.py
+++ src/spatialdata_plot/pl/basic.py
@@ -33,7 +33,7 @@ class PlotAccessor:
         """
         if num_images <= 1:
             raise ValueError("Number of images must be greater than 1.")
-        
+
         if num_images < ncols:
             nrows = 1
             ncols = num_images
@@ -51,7 +51,9 @@ class PlotAccessor:
         _ = [ax.axis("off") for ax in axes.flatten()[num_images:]]
         return fig, axes
 
-    def imshow(self, ax=None, ncols=4, width=4, height=3, **kwargs) -> sd.SpatialData:
+    def imshow(
+        self, ax: Union[plt.Axes, None] = None, ncols: int = 4, width: int = 4, height: int = 3, **kwargs
+    ) -> sd.SpatialData:
         """
         Plot the images in the SpatialData object.
 
@@ -95,8 +97,80 @@ class PlotAccessor:
 
         return self._sdata
 
-    def test_plot(self):
-        plt.plot(np.arange(10), np.arange(10))
+    def scatter(
+        self,
+        x: str,
+        y: str,
+        color: Union[str, None] = None,
+        ax: plt.Axes = None,
+        ncols: int = 4,
+        width: int = 4,
+        height: int = 3,
+        **kwargs,
+    ) -> sd.SpatialData:
+        """Plots a scatter plot of observations in the table of a SpatialData object.
 
-    def scatter(self):
-        plt.scatter(np.random.randn(20), np.random.randn(20))
+        Parameters
+        ----------
+        x : str
+            Column name of the x-coordinates.
+        y : str
+            Column name of the y-coordinates.
+        color : str, optional
+            Column name of the color, by default None.
+        ax : matplotlib.axes.Axes, optional
+            Matplotlib axes object to plot on. If None, a new figure is created.
+            Only works if there is one image in the SpatialData object.
+        ncols : int, optional
+            Number of columns in the figure. Default is 4.
+        width : int, optional
+            Width of each subplot. Default is 4.
+        height : int, optional
+            Height of each subplot. Default is 3.
+        kwargs : dict
+            Additional keyword arguments to pass to the scatter plot.
+
+        Returns
+        -------
+        sd.SpatialData
+            A SpatialData object.
+        """
+        image_data = self._sdata.images
+        region_key = self._sdata.pp.get_region_key()
+        regions = self._sdata.table.obs[region_key].unique().tolist()
+
+        region_mapping = {k.split("/")[-1]: k for k in regions}
+
+        for k in image_data.keys():
+            if k not in region_mapping.keys():
+                del region_mapping[k]
+
+        num_images = len(region_mapping)
+
+        if num_images == 1:
+            ax = ax or plt.gca()
+            # TODO: support for labels instead of colors (these can be cell types for example) => Automatic coloring.
+            if color is not None:
+                kwargs["c"] = self._sdata.table.obs[color]
+            ax.scatter(self._sdata.table.obs[x], self._sdata.table.obs[y], **kwargs)
+            key = [k for k in image_data.keys()][0]
+            ax.set_title(key)
+            ax.margins(x=0.01)
+            ax.margins(y=0.01)
+
+        if num_images > 1:
+            fig, axes = self._subplots(num_images, ncols, width, height)
+
+            for i, (ax, (k, v)) in enumerate(zip(np.ravel(axes), region_mapping.items())):
+                if i < num_images:
+                    sub_table = self._sdata.table.obs[self._sdata.table.obs[region_key] == v]
+
+                    if color is not None:
+                        kwargs["c"] = sub_table[color]
+
+                    ax.scatter(sub_table[x], sub_table[y], **kwargs)
+                    ax.set_title(k)
+                    ax.margins(x=0.01)
+                    ax.margins(y=0.01)
+
+        return self._sdata
