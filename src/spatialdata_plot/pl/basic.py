3def211e304f301a52324968d3be5fc34c4cf5cb
Harald Vöhringer <harald.voeh@gmail.com>
2023-02-16 19:13:10 +0100

    Feature/202302 refactor plotting (#7)
    * Refactored subplots logic to pl._subplots
    
    * added docstrings, fixed subplots edge case if num_images < ncols.
    
    * basic implementation pl.scatter
    
    ---------
    
    Co-authored-by: Harald Sager Voehringer <voehring@login02.cluster.embl.de>

--- src/spatialdata_plot/pl/basic.py
+++ src/spatialdata_plot/pl/basic.py
@@ -1,4 +1,7 @@
+from typing import Union
+
 import numpy as np
+import spatialdata as sd
 from matplotlib import pyplot as plt
 
 from ..accessor import register_spatial_data_accessor
@@ -9,35 +12,165 @@ class PlotAccessor:
     def __init__(self, sdata):
         self._sdata = sdata
 
-    def imshow(self, ax=None, ncols=4, width=4, height=3, **kwargs):
+    def _subplots(
+        self, num_images: int, ncols: int = 4, width: int = 4, height: int = 3
+    ) -> Union[plt.Figure, plt.Axes]:
+        """Helper function to set up axes for plotting.
+
+        Parameters
+        ----------
+        num_images : int
+            Number of images to plot. Must be greater than 1.
+        ncols : int, optional
+            Number of columns in the subplot grid, by default 4
+        width : int, optional
+            Width of each subplot, by default 4
+
+        Returns
+        -------
+        Union[plt.Figure, plt.Axes]
+            Matplotlib figure and axes object.
+        """
+        if num_images <= 1:
+            raise ValueError("Number of images must be greater than 1.")
+
+        if num_images < ncols:
+            nrows = 1
+            ncols = num_images
+        else:
+            nrows, reminder = divmod(num_images, ncols)
+
+            if nrows == 0:
+                nrows = 1
+            if reminder > 0:
+                nrows += 1
+
+        fig, axes = plt.subplots(nrows, ncols, figsize=(width * ncols, height * nrows))
+
+        # get rid of the empty axes
+        _ = [ax.axis("off") for ax in axes.flatten()[num_images:]]
+        return fig, axes
+
+    def imshow(
+        self, ax: Union[plt.Axes, None] = None, ncols: int = 4, width: int = 4, height: int = 3, **kwargs
+    ) -> sd.SpatialData:
+        """
+        Plot the images in the SpatialData object.
+
+        Parameters
+        ----------
+        ax : matplotlib.axes.Axes, optional
+            Matplotlib axes object to plot on. If None, a new figure is created.
+            Works only if there is one image in the SpatialData object.
+        ncols : int, optional
+            Number of columns in the figure. Default is 4.
+        width : int, optional
+            Width of each subplot. Default is 4.
+        height : int, optional
+            Height of each subplot. Default is 3.
+
+        Returns
+        -------
+        sd.SpatialData
+            A SpatialData object.
+        """
         image_data = self._sdata.images
         num_images = len(image_data)
+
+        if num_images == 0:
+            raise ValueError("No images found in the SpatialData object.")
 
         if num_images == 1:
             ax = ax or plt.gca()
             key = [k for k in image_data.keys()][0]
             ax.imshow(image_data[key].values.T)
             ax.set_title(key)
-        else:
-            nrows, reminder = divmod(num_images, ncols)
-            if reminder > 0:
-                nrows += 1
 
-            fig, axes = plt.subplots(nrows, ncols, figsize=(ncols * width, nrows * height))
+        if num_images > 1:
+            fig, axes = self._subplots(num_images, ncols, width, height)
 
+            # iterate over each image and plot it onto the axes
             for i, (ax, (k, v)) in enumerate(zip(np.ravel(axes), image_data.items())):
                 if i < num_images:
                     ax.imshow(v.values.T)
                     ax.set_title(k)
 
-            # get rid of the empty axes
-            for i in range(num_images, ncols * nrows):
-                axes.ravel()[i].axis("off")
-
         return self._sdata
 
-    def test_plot(self):
-        plt.plot(np.arange(10), np.arange(10))
+    def scatter(
+        self,
+        x: str,
+        y: str,
+        color: Union[str, None] = None,
+        ax: plt.Axes = None,
+        ncols: int = 4,
+        width: int = 4,
+        height: int = 3,
+        **kwargs,
+    ) -> sd.SpatialData:
+        """Plots a scatter plot of observations in the table of a SpatialData object.
 
-    def scatter(self):
-        plt.scatter(np.random.randn(20), np.random.randn(20))
+        Parameters
+        ----------
+        x : str
+            Column name of the x-coordinates.
+        y : str
+            Column name of the y-coordinates.
+        color : str, optional
+            Column name of the color, by default None.
+        ax : matplotlib.axes.Axes, optional
+            Matplotlib axes object to plot on. If None, a new figure is created.
+            Only works if there is one image in the SpatialData object.
+        ncols : int, optional
+            Number of columns in the figure. Default is 4.
+        width : int, optional
+            Width of each subplot. Default is 4.
+        height : int, optional
+            Height of each subplot. Default is 3.
+        kwargs : dict
+            Additional keyword arguments to pass to the scatter plot.
+
+        Returns
+        -------
+        sd.SpatialData
+            A SpatialData object.
+        """
+        image_data = self._sdata.images
+        region_key = self._sdata.pp.get_region_key()
+        regions = self._sdata.table.obs[region_key].unique().tolist()
+
+        region_mapping = {k.split("/")[-1]: k for k in regions}
+
+        for k in image_data.keys():
+            if k not in region_mapping.keys():
+                del region_mapping[k]
+
+        num_images = len(region_mapping)
+
+        if num_images == 1:
+            ax = ax or plt.gca()
+            # TODO: support for labels instead of colors (these can be cell types for example) => Automatic coloring.
+            if color is not None:
+                kwargs["c"] = self._sdata.table.obs[color]
+            ax.scatter(self._sdata.table.obs[x], self._sdata.table.obs[y], **kwargs)
+            key = [k for k in image_data.keys()][0]
+            ax.set_title(key)
+            ax.margins(x=0.01)
+            ax.margins(y=0.01)
+
+        if num_images > 1:
+            fig, axes = self._subplots(num_images, ncols, width, height)
+
+            for i, (ax, (k, v)) in enumerate(zip(np.ravel(axes), region_mapping.items())):
+                if i < num_images:
+                    sub_table = self._sdata.table.obs[self._sdata.table.obs[region_key] == v]
+
+                    if color is not None:
+                        kwargs["c"] = sub_table[color]
+
+                    ax.scatter(sub_table[x], sub_table[y], **kwargs)
+                    ax.set_title(k)
+                    ax.margins(x=0.01)
+                    ax.margins(y=0.01)
+
+        return self._sdata
