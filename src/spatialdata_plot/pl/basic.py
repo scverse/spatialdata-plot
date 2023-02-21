5ccd21e004db3a841918625a0e2b8dfc9ff1e69d
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-02-21 19:00:58 +0100

    reimplemented infill plotting

--- src/spatialdata_plot/pl/basic.py
+++ src/spatialdata_plot/pl/basic.py
@@ -1,9 +1,15 @@
-from typing import Union
+from typing import Union, Optional
 from collections import OrderedDict
-
+from matplotlib.colors import Colormap, BoundaryNorm
 import numpy as np
 import spatialdata as sd
-from matplotlib import pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap, to_hex
+from matplotlib.cm import get_cmap
+from anndata import AnnData
+import pandas as pd
+
 
 from ..accessor import register_spatial_data_accessor
 
@@ -12,6 +18,57 @@ from ..accessor import register_spatial_data_accessor
 class PlotAccessor:
     def __init__(self, sdata):
         self._sdata = sdata
+
+    def _copy(
+        self,
+        images: Union[None, dict] = None,
+        labels: Union[None, dict] = None,
+        points: Union[None, dict] = None,
+        shapes: Union[None, dict] = None,
+        table: Union[dict, AnnData] = None,
+    ) -> sd.SpatialData:
+
+        """
+        Helper function to copies the references from the original SpatialData
+        object to the subsetted SpatialData object.
+        """
+
+        return sd.SpatialData(
+            images=self._sdata.images if images is None else images,
+            labels=self._sdata.labels if labels is None else labels,
+            points=self._sdata.points if points is None else points,
+            shapes=self._sdata.shapes if shapes is None else shapes,
+            table=self._sdata.table if table is None else table,
+        )
+
+    def _get_region_key(self) -> str:
+
+        "Quick access to the data's region key."
+
+        if not hasattr(self._sdata, "table"):
+            raise ValueError("SpatialData object does not have a table.")
+
+        return self._sdata.table.uns["spatialdata_attrs"]["region_key"]
+
+    def _get_instance_key(self) -> str:
+
+        if not hasattr(self._sdata, "table"):
+            raise ValueError("SpatialData object does not have a table.")
+
+        "Quick access to the data's instance key."
+
+        return self._sdata.table.uns["spatialdata_attrs"]["instance_key"]
+
+    def _verify_plotting_tree_exists(self):
+
+        if not hasattr(self._sdata, "table"):
+            raise ValueError("SpatialData object does not have a table.")
+
+        if not hasattr(self._sdata.table, "uns"):
+            raise ValueError("Table in SpatialData object does not have a 'uns' attribute.")
+
+        if "plotting_tree" not in self._sdata.table.uns.keys():
+            self._sdata.table.uns["plotting_tree"] = OrderedDict()
 
     def _subplots(
         self, num_images: int, ncols: int = 4, width: int = 4, height: int = 3
@@ -32,8 +89,8 @@ class PlotAccessor:
         Union[plt.Figure, plt.Axes]
             Matplotlib figure and axes object.
         """
-        if num_images <= 1:
-            raise ValueError("Number of images must be greater than 1.")
+        # if num_images <= 1:
+        # raise ValueError("Number of images must be greater than 1.")
 
         if num_images < ncols:
             nrows = 1
@@ -49,8 +106,90 @@ class PlotAccessor:
         fig, axes = plt.subplots(nrows, ncols, figsize=(width * ncols, height * nrows))
 
         # get rid of the empty axes
-        _ = [ax.axis("off") for ax in axes.flatten()[num_images:]]
+        # _ = [ax.axis("off") for ax in axes.flatten()[num_images:]]
         return fig, axes
+
+    def render_labels(
+        self,
+        cell_key: str,
+        colour_key: Optional[Union[str, None]] = None,
+        border_alpha: float = 1.0,
+        border_colour: Optional[Union[str, None]] = None,
+        fill_alpha: float = 1.0,
+        fill_colour: Optional[Union[str, None]] = None,
+        mode: str = "inner",
+        bg_colour: Optional[Union[str, None]] = None,
+        cmap: Optional[Union[str, Colormap]] = matplotlib.pyplot.cm.gist_rainbow,
+    ) -> matplotlib.pyplot.Axes:
+
+        if not isinstance(cell_key, str):
+            raise TypeError("Parameter 'cell_key' must be a string.")
+
+        if cell_key not in self._sdata.table.obs.columns:
+            raise ValueError(f"The provided cell_key '{cell_key}' is not a valid table column.")
+
+        if colour_key is not None:
+
+            if not isinstance(colour_key, (str, type(None))):
+                raise TypeError("Parameter 'colour_key' must be a string.")
+
+            if colour_key not in self._sdata.table.obs.columns:
+                raise ValueError(f"The provided colour_key '{colour_key}' is not a valid table column.")
+
+        if not isinstance(border_alpha, (int, float)):
+            raise TypeError("Parameter 'border_alpha' must be a float.")
+
+        if not (border_alpha <= 1 and border_alpha >= 0):
+            raise ValueError("Parameter 'border_alpha' must be between 0 and 1.")
+
+        if border_colour is not None:
+
+            if not isinstance(colour_key, (str, type(None))):
+                raise TypeError("If specified, parameter 'border_colour' must be a string.")
+
+        if not isinstance(fill_alpha, (int, float)):
+            raise TypeError("Parameter 'fill_alpha' must be a float.")
+
+        if not (fill_alpha <= 1 and fill_alpha >= 0):
+            raise ValueError("Parameter 'fill_alpha' must be between 0 and 1.")
+
+        if fill_colour is not None:
+
+            if not isinstance(fill_colour, (str, type(None))):
+                raise TypeError("If specified, parameter 'fill_colour' must be a string.")
+
+        valid_modes = ["thick", "inner", "outer", "subpixel"]
+        if not isinstance(mode, str):
+            raise TypeError("Parameter 'mode' must be a string.")
+
+        if mode not in valid_modes:
+            raise ValueError("Parameter 'mode' must be one of 'thick', 'inner', 'outer', 'subpixel'.")
+
+        if bg_colour is not None:
+
+            if not isinstance(bg_colour, (str, type(None))):
+                raise TypeError("If specified, parameter 'bg_colour' must be a string.")
+
+        # TODO(ttreis): Steal cmap type checking from squidpy
+
+        self._verify_plotting_tree_exists()
+
+        # get current number of steps to create a unique key
+        table = self._sdata.table.copy()
+        n_steps = len(table.uns["plotting_tree"].keys())
+        table.uns["plotting_tree"][f"{n_steps+1}_render_labels"] = {
+            "cell_key": cell_key,
+            "colour_key": colour_key,
+            "border_alpha": border_alpha,
+            "border_colour": border_colour,
+            "fill_alpha": fill_alpha,
+            "fill_colour": fill_colour,
+            "mode": mode,
+            "bg_colour": bg_colour,
+            "cmap": cmap,
+        }
+
+        return self._copy(table=table)
 
     def _render_images(self, params, axs):
 
@@ -67,6 +206,83 @@ class PlotAccessor:
     def _render_points(self, params, axs):
 
         pass
+
+    def _render_labels(self, params, key: str, ax):
+
+        instance_key = self._get_instance_key()
+        region_key = self._get_region_key()
+
+        # First plot the infill, if desired
+        if params["fill_alpha"] != 0:
+
+            pass
+
+        # 1) Get matching of regions to images
+        regions = self._sdata.table.obs[region_key].unique().tolist()
+        region_mapping = {k.split("/")[-1]: k for k in regions}
+
+        for k in self._sdata.images.keys():
+            if k not in region_mapping.keys():
+                del region_mapping[k]
+
+        # 2) First identify colourmap, then create discrete colours from it
+        if isinstance(params["cmap"], str):
+            try:
+                cmap = get_cmap(params["cmap"])
+            except ValueError:
+                raise ValueError(f"Colormap '{params['cmap']}' not found.")
+        elif isinstance(params["cmap"], matplotlib.colors.Colormap):
+            cmap = params["cmap"]
+        else:
+            raise TypeError("Parameter 'cmap' must be a string or a matplotlib.colors.Colormap.")
+
+        groups = self._sdata.table.obs[params["colour_key"]].unique()
+
+        if len(groups) > 256:
+            raise ValueError("Too many colours needed for plotting.")
+
+        colors = ListedColormap(cmap(np.linspace(0, 1, len(groups) + 1)))
+        group_to_colour = pd.DataFrame(
+            {params["colour_key"]: groups, "color": [color for color in colors(range(len(groups)))]}
+        )
+        cell_to_group = pd.DataFrame(
+            self._sdata.table.obs[[params["cell_key"], params["colour_key"]]].values,
+            columns=[params["cell_key"], params["colour_key"]],
+        )
+        cell_to_colour = cell_to_group.merge(group_to_colour, on=params["colour_key"], how="left").drop(
+            params["colour_key"], axis=1
+        )
+
+        # apply alpha to background
+        cell_to_colour.color = [[c[0], c[1], c[2], params["fill_alpha"]] for c in cell_to_colour.color]
+
+        # 3) Plot each image with segmentation
+
+        seg = self._sdata.labels[key].to_numpy()
+        values = np.unique(seg)
+
+        # Create a list of colors corresponding to each integer value
+        color_list = []
+        for v in values:
+            color = (
+                cell_to_colour.loc[cell_to_colour[params["cell_key"]] == v, "color"].iloc[0]
+                if v in cell_to_colour[params["cell_key"]].values
+                else params["bg_colour"]
+            )
+            color_list.append(color)
+
+        cmap = ListedColormap(color_list)
+        cmap.set_bad(color=params["bg_colour"])
+        bounds = np.arange(len(values) + 1)
+        norm = BoundaryNorm(bounds, cmap.N)
+
+        ax.imshow(seg, cmap=cmap, norm=norm, interpolation="nearest")
+        
+        ax.set_title(key)
+        ax.set_xlabel("spatial1")
+        ax.set_ylabel("spatial2")
+        ax.set_xticks([])
+        ax.set_yticks([])
 
     def imshow(
         self, ax: Union[plt.Axes, None] = None, ncols: int = 4, width: int = 4, height: int = 3, **kwargs
@@ -107,6 +323,7 @@ class PlotAccessor:
             "render_channels",
             "render_shapes",
             "render_points",
+            "render_labels",
         ]
 
         if (
@@ -154,17 +371,10 @@ class PlotAccessor:
 
             # set up canvas
             num_images = len(self._sdata.images.keys())
-
-            if num_images == 1:
-                axs = ax or plt.gca()
-
-            if num_images > 1:
-                fig, axs = self._subplots(num_images, ncols, width, height)
+            fig, axs = self._subplots(num_images, ncols, width, height)
 
             # go through tree
             for cmd, params in render_cmds.items():
-
-                cmd = "_".join(cmd.split("_")[1:])
 
                 if cmd == "render_images":
 
@@ -180,8 +390,21 @@ class PlotAccessor:
 
                 elif cmd == "render_points":
 
-                    self._render_points(params, axs)
-                    
+                    for ax in axs:
+
+                        self._render_points(params, ax)
+
+                elif cmd == "render_labels":
+
+                    for idx, ax in enumerate(axs):
+
+                        key = list(self._sdata.labels.keys())[idx]
+                        self._render_labels(params=params, key=key, ax=ax)
+
+                else:
+
+                    raise NotImplementedError(f"Command '{cmd}' is not supported.")
+
         return fig, axs
 
     def scatter(
