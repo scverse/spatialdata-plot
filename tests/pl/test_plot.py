81ae23756eb5e06801c48d5aa71ca13e8f055d0b
giovp <giov.pll@gmail.com>
2023-04-04 18:35:41 +0200

    add test comparison for plots

---
+++ tests/pl/test_plot.py
@@ -0,0 +1,27 @@
+import scanpy as sc
+from spatialdata import SpatialData
+
+import spatialdata_plot  # noqa: F401
+from tests.conftest import PlotTester, PlotTesterMeta
+
+sc.pl.set_rcParams_defaults()
+sc.set_figure_params(dpi=40, color_map="viridis")
+
+# WARNING:
+# 1. all classes must both subclass PlotTester and use metaclass=PlotTesterMeta
+# 2. tests which produce a plot must be prefixed with `test_plot_`
+# 3. if the tolerance needs to be changed, don't prefix the function with `test_plot_`, but with something else
+#    the comp. function can be accessed as `self.compare(<your_filename>, tolerance=<your_tolerance>)`
+#    ".png" is appended to <your_filename>, no need to set it
+
+
+class TestLabels(PlotTester, metaclass=PlotTesterMeta):
+    def test_tol_plot_labels(self, sdata_blobs: SpatialData):
+        sdata_blobs.pl.render_labels().pl.show()
+
+        self.compare("Labels_labels", tolerance=30)
+
+
+class TestImages(PlotTester, metaclass=PlotTesterMeta):
+    def test_plot_images(self, sdata_blobs: SpatialData):
+        sdata_blobs.pl.render_images().pl.show()
