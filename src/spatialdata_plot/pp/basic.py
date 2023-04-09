bf5d49619451302d3d8259353440c7f7089ef3de
Harald Vohringer <harald.voeh@gmail.com>
2023-04-09 15:14:25 +0200

    fixed build docs by removing attributes docstrings in Preprocessing and Plotting accessor

--- src/spatialdata_plot/pp/basic.py
+++ src/spatialdata_plot/pp/basic.py
@@ -18,6 +18,14 @@ from ..pp.utils import _get_region_key, _verify_plotting_tree_exists
 
 @register_spatial_data_accessor("pp")
 class PreprocessingAccessor:
+    """
+    Preprocessing functions for SpatialData objects.
+
+    Parameters
+    ----------
+    sdata :
+        A spatial data object.
+    """
 
     @property
     def sdata(self) -> sd.SpatialData:
@@ -29,20 +37,6 @@ class PreprocessingAccessor:
         self._sdata = sdata
 
     def __init__(self, sdata: sd.SpatialData) -> None:
-        """
-        Preprocessing functions for SpatialData objects.
-
-        Parameters
-        ----------
-        sdata :
-            A spatial data object.
-
-        Attributes
-        ----------
-        sdata :
-            A spatial data object.
-        """
-
         self._sdata = sdata
 
     def _copy(
