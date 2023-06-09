e5bcb0e489bae20ca26e26bd1b2af4426ff04ddf
Tim Treis <tim.treis@stud.uni-heidelberg.de>
2023-06-09 14:53:04 +0200

    Adds `transfunc` parameter to optionally transform continuous values when coloring (#85)

--- src/spatialdata_plot/pl/render.py
+++ src/spatialdata_plot/pl/render.py
@@ -4,7 +4,7 @@ from collections.abc import Sequence
 from copy import copy
 from dataclasses import dataclass
 from functools import partial
-from typing import Any, Optional, Union
+from typing import Any, Callable, Optional, Union
 
 import matplotlib
 import numpy as np
@@ -58,6 +58,7 @@ class ShapesRenderParams:
     outline_alpha: float = 1.0
     fill_alpha: float = 0.3
     size: float = 1.0
+    transfunc: Callable[[float], float] | None = None
 
 
 def _render_shapes(
@@ -102,6 +103,10 @@ def _render_shapes(
         alpha=render_params.fill_alpha,
     )
 
+    # color_source_vector is None when the values aren't categorical
+    if color_source_vector is None and render_params.transfunc is not None:
+        color_vector = render_params.transfunc(color_vector)
+
     def _get_collection_shape(
         shapes: list[GeoDataFrame],
         c: Any,
@@ -127,8 +132,11 @@ def _render_shapes(
             # fails when numeric
             fill_c = ColorConverter().to_rgba_array(c)
         except ValueError:
-            norm = colors.Normalize(vmin=min(c), vmax=max(c))
-            c = cmap(norm(c))
+            if norm is None:
+                c = cmap(c)
+            else:
+                norm = colors.Normalize(vmin=min(c), vmax=max(c))
+                c = cmap(norm(c))
 
         fill_c = ColorConverter().to_rgba_array(c)
         fill_c[..., -1] = render_params.fill_alpha
@@ -204,6 +212,7 @@ class PointsRenderParams:
     palette: Palette_t = None
     alpha: float = 1.0
     size: float = 1.0
+    transfunc: Callable[[float], float] | None = None
 
 
 def _render_points(
@@ -256,6 +265,10 @@ def _render_points(
         na_color=render_params.cmap_params.na_color,
         alpha=render_params.alpha,
     )
+
+    # color_source_vector is None when the values aren't categorical
+    if color_source_vector is None and render_params.transfunc is not None:
+        color_vector = render_params.transfunc(color_vector)
 
     norm = copy(render_params.cmap_params.norm)
     _cax = ax.scatter(
@@ -386,6 +399,7 @@ class LabelsRenderParams:
     palette: Palette_t = None
     outline_alpha: float = 1.0
     fill_alpha: float = 0.4
+    transfunc: Callable[[float], float] | None = None
 
 
 def _render_labels(
