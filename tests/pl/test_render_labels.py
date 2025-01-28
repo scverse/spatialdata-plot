39038e913fa9b09c732c399ac4855fad1d802643
ArneDefauw <arnedefauw@gmail.com>
2025-01-28 11:53:48 +0100

    matplotlib

--- tests/pl/test_render_labels.py
+++ tests/pl/test_render_labels.py
@@ -16,7 +16,7 @@ from tests.conftest import DPI, PlotTester, PlotTesterMeta
 RNG = np.random.default_rng(seed=42)
 sc.pl.set_rcParams_defaults()
 sc.set_figure_params(dpi=DPI, color_map="viridis")
-#matplotlib.use("agg")  # same as GitHub action runner
+matplotlib.use("agg")  # same as GitHub action runner
 _ = spatialdata_plot
 
 # WARNING:
