19283ff48e8a66214d6f6b96476acd841eb2e86c
Matthias Fabian Meyer-Bender <matthias.meyerbender@embl.de>
2025-12-19 10:12:51 +0100

    fix test imports

--- tests/pl/test_logging.py
+++ tests/pl/test_logging.py
@@ -1,10 +1,11 @@
 import logging
 
 import spatialdata_plot
-
+from spatialdata import SpatialData
+from tests.conftest import PlotTester, PlotTesterMeta
 
 class TestLogging(PlotTester, metaclass=PlotTesterMeta):
-    def test_default_verbosity_hides_info(self, sdata_blobs: "SpatialData", caplog):
+    def test_default_verbosity_hides_info(self, sdata_blobs: SpatialData, caplog):
         """INFO logs should be hidden by default."""
         caplog.set_level(logging.INFO, logger=spatialdata_plot._logging.logger.name)
 
@@ -14,7 +15,7 @@ class TestLogging(PlotTester, metaclass=PlotTesterMeta):
         # make sure no INFO messages were recorded
         assert all(record.levelno != logging.INFO for record in caplog.records)
 
-    def test_verbose_verbosity_shows_info(self, sdata_blobs: "SpatialData", caplog):
+    def test_verbose_verbosity_shows_info(self, sdata_blobs: SpatialData, caplog):
         """INFO logs should appear when verbose=True."""
         spatialdata_plot.set_verbosity(True)
         caplog.set_level(logging.INFO, logger=spatialdata_plot._logging.logger.name)
